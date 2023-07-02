a40f995a7b67d0843471ae61e0cda5e20004e9d4
yos <yosef.debela@ju.edu.et>
2023-07-02 22:37:24 +0300

    com

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+        print(bullets.update)
         gf.update_screen(ai_settings, screen, ship, bullets)
         # ship.blitme()
         # pygame.display.flip()
