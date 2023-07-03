9b8791b0809ff8bdb867b2b4f499e14626bcc5d2
yos <yosef.debela@ju.edu.et>
2023-07-03 21:00:52 +0300

    bullet speed decreased

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-# import sys
-import pygame
-from pygame.sprite import Group
-from setting import Settings
-from ship import Ship
-
-import game_functions as gf
-
-
-def run_game():
-    # initializing pygame
-    pygame.init()
-    ai_settings = Settings()
-    # this make ai_setting an instance of setting class
-
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.scree_height))
-    pygame.display.set_caption("Alien Invasion")
-    # make a ship
-    ship = Ship(ai_settings, screen)
-    # make a group to store bullet in
-    bullets = Group()  
-
-    while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        print(bullets.update)
-        gf.update_screen(ai_settings, screen, ship, bullets)
-        # ship.blitme()
-        # pygame.display.flip()
-        """this function will continually update the screen display"""
-run_game()
-=======
 # import sys
 import pygame
 from pygame.sprite import Group
@@ -67,5 +33,4 @@ def run_game():
         # pygame.display.flip()
         """this function will continually update the screen display"""
 run_game()
->>>>>>> refs/remotes/origin/main
 #coment added
--- setting.py
+++ setting.py
@@ -7,7 +7,7 @@ class Settings():
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
         # bullet settings
-        self.bullet_speed_factor = 3
+        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
